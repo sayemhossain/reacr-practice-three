00aae61ea7f016deda82f7371371993e3ba538dd
SAYAM HOSSAIN <shayem15-12011@diu.edu.bd>
2022-03-21 20:15:40 +0600

    new code added

--- src/Component/Cosmetic/Cosmetic.js
+++ src/Component/Cosmetic/Cosmetic.js
@@ -1,7 +1,15 @@
 import React from "react";
 
-const Cosmetic = () => {
-  return <div></div>;
+const Cosmetic = (props) => {
+  console.log(props);
+  const { name, price, id } = props.cosmetic;
+  return (
+    <div>
+      <h2>Poduct Name: {name} </h2>
+      <p>Only for: {price} </p>
+      <p>ID: {id}</p>
+    </div>
+  );
 };
 
 export default Cosmetic;
--- src/Component/Cosmetics/Cosmetics.js
+++ src/Component/Cosmetics/Cosmetics.js
@@ -1,9 +1,22 @@
 import React from "react";
+import { useEffect } from "react";
+import { useState } from "react";
+import Cosmetic from "../Cosmetic/Cosmetic";
 
 const Cosmetics = () => {
+  const [cosmetics, setCosmetics] = useState([]);
+  useEffect(() => {
+    fetch("data.json")
+      .then((res) => res.json())
+      .then((data) => setCosmetics(data));
+  }, []);
+
   return (
     <div>
-      <h1>This is Cosmetics section</h1>
+      <h1>Welcome to my Cosmetics Store</h1>
+      {cosmetics.map((cosmetic) => (
+        <Cosmetic key={cosmetic.id} cosmetic={cosmetic}></Cosmetic>
+      ))}
     </div>
   );
 };

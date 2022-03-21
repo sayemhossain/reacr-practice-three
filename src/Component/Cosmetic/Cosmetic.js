f2afef4e731316d38b4e00780d32b9b264ab4f3f
SAYAM HOSSAIN <shayem15-12011@diu.edu.bd>
2022-03-21 20:36:51 +0600

    new code

--- src/Component/Cosmetic/Cosmetic.js
+++ src/Component/Cosmetic/Cosmetic.js
@@ -2,13 +2,23 @@ import React from "react";
 import "./Cosmetic.css";
 
 const Cosmetic = (props) => {
-  console.log(props);
   const { name, price, id } = props.cosmetic;
+  const addToCart = (id) => {
+    console.log("Item added", id);
+  };
   return (
     <div className="single-cosmetic">
       <h2>Name: {name} </h2>
       <p>Only for: ${price} </p>
       <p>ID: {id}</p>
+      <button
+        onClick={() => {
+          addToCart(id);
+        }}
+      >
+        Add to cart
+      </button>
+      <button>Remove cart</button>
     </div>
   );
 };
